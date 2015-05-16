1506449a87e2e407143a112e499a537f15419079
Daryl Schroeder <daryl.schroeder@intergraph.com>
2015-05-16 08:25:46 +0000

    Changes made by Daryl Schroeder (daryl.schroeder@intergraph.com).

--- MOBR-KendoDrawer-JS/scripts/PBSHandler.js
+++ MOBR-KendoDrawer-JS/scripts/PBSHandler.js
@@ -1,3 +1,52 @@
-
 (function () {
-})
+    var currentPBSItem;
+    window.PBSItemDetail = {
+        show: function () {
+            // //Pull the ISBN number from the query string
+            //var location = window.location.toString();
+			//alert(SelectedPBS.Name);
+            // window.ItemDetails = {
+            //     "name": SelectedPBS.Name;
+            // }
+            //var id = location.substring(location.lastIndexOf('?') + 4);
+	
+            //window.CurrentID=id;
+            //alert(window.pbs.ds.data.view()[0].uid);
+            // window.pbs.data.filter({
+            //     field: "Name",
+            //     operator: "eq",
+            //     value: id
+            // });
+            // currentPBSItem = window.pbs.data.view()[0];
+            // alert(currentPBSItem.Name);
+            // // Filter the DataSource bt ISBN to get the selected record
+            // window.Books.data.filter({
+            //     field: "isbn",
+            //     operator: "eq",
+            //     value: isbn
+            // });
+            // currentBook = window.Books.data.view()[0];
+            // // Create a model for the page and bind it to the view
+            // var book = {
+            //     title: currentBook.name + " by " + currentBook.author,
+            //     image_url: currentBook.image_url,
+            //     amazon_url: currentBook.amazon_url,
+            //     is_favorite: currentBook.is_favorite
+            // };
+            // kendo.bind($('#bookContent'), book, kendo.mobile.ui);
+            // // If the current book is a favorited item, toggle the switch on the view
+            // if (currentBook.is_favorite) {
+            //     $('#favorite').data('kendoMobileSwitch').toggle();
+            // }
+        },
+        hide: function () {
+            // When the user navigates away from the page, remove the filter
+            //window.Books.data.filter([]);
+        },
+        openLink: function () {
+            // Will use the Cordova InAppBrowser plugin when deployed to a device. Opens a new window in
+            // the simulator
+            //window.open(currentBook.amazon_url, '_blank', 'location=yes');
+        }
+    };
+}());
--- MOBR-KendoDrawer-JS/scripts/app.js
+++ MOBR-KendoDrawer-JS/scripts/app.js
@@ -1,3 +1,18 @@
+var currentTheme = "glacier";
+
+function toggleTheme() {
+    var themes = ["glacier", "shadow"];
+    for (var i = 0; i < themes.length; i++) {
+        var styleSheet = document.getElementById(themes[i]);
+        if (themes[i] != currentTheme) {
+            styleSheet.removeAttribute("disabled");
+            newTheme = themes[i];
+        } else {
+            styleSheet.setAttribute("disabled", "disabled");
+        }
+    }
+    currentTheme = newTheme;
+}
 
 (function () {
 
@@ -5,60 +20,60 @@
     // later on so that we can use it if need be
     var app;
 
-		
+
     var apiKey = "Hro5ZCMacvvdbWuA";
     var el = new Everlive(apiKey);
     // create an object to store the models for each view
     window.APP = {
-      models: {
-        home: {
-          title: 'Home'
-        },
-        settings: {
-          title: 'Settings'
-        },
-        pbs: {
-          title: 'PBS',
-          ds: new kendo.data.DataSource({
-              
-                transport: {
-                    read: {
-                        url: "https://api.everlive.com/v1/" + apiKey + "/PBSLevel",
-                        dataType: "jsonp"
+        models: {
+            home: {
+                title: 'Home'
+            },
+            settings: {
+                title: 'Settings'
+            },
+            pbs: {
+                title: 'PBS',
+                ds: new kendo.data.DataSource({
+                    transport: {
+                        read: {
+                            url: "https://api.everlive.com/v1/" + apiKey + "/PBSLevel",
+                            dataType: "jsonp"
+                        }
+                    },
+                    schema: {
+                        data: function (response) {
+                            return response.Result;
+                        }
                     }
-                },
-                schema: {
-                    data: function (response) {
-                        return response.Result;
-                    }
+                }),
+                details_pbs: function (e) {
+                    app.navigate("views/pbsItemDetails.html", "slide");
+                    SelectedPBS = e.data;
                 }
-            }),
-          alert: function(e) {
-            alert(e.data.Name);
-          }
+            }
         }
-      }
     };
-    
-    
+
+
     // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
-      
-      // hide the splash screen as soon as the app is ready. otherwise
-      // Cordova will wait 5 very long seconds to do it for you.
-      navigator.splashscreen.hide();
+    document.addEventListener('deviceready', function () {
 
-      app = new kendo.mobile.Application(document.body, {
-        
-        // comment out the following line to get a UI which matches the look
-        // and feel of the operating system
-        skin: 'native',
+        // hide the splash screen as soon as the app is ready. otherwise
+        // Cordova will wait 5 very long seconds to do it for you.
+        navigator.splashscreen.hide();
 
-        // the application needs to know which view to load first
-        initial: 'views/home.html'
-      });
+        app = new kendo.mobile.Application(document.body, {
+
+            // comment out the following line to get a UI which matches the look
+            // and feel of the operating system
+            skin: 'native',
+            transition: 'slide',
+
+            // the application needs to know which view to load first
+            initial: 'views/home.html'
+        });
 
     }, false);
-
 
 }());
