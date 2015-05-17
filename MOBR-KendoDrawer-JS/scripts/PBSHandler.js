295b8d6d4d61614f4cfbe630f2a9521820aff41a
Daryl Schroeder <daryl.schroeder@intergraph.com>
2015-05-17 00:18:28 +0000

    Changes made by Daryl Schroeder (daryl.schroeder@intergraph.com).

--- MOBR-KendoDrawer-JS/scripts/PBSHandler.js
+++ MOBR-KendoDrawer-JS/scripts/PBSHandler.js
@@ -1,52 +1,93 @@
-(
+function showPBSItemDetail() {
+        //TODO: use callbacks here so we can make sure the data is fully set before we bind.
 
+        console.log("TRACE: [PBSItemDetail.show] Enter");
 
-    function () {
-        var currentPBSItem;
-        window.PBSItemDetail = {
-            show: function () {
+        // -- build up the query for tags -- 
+        var expandTags = {
+            "PBSItemTag": true
+        };
+        var queryTags = new Everlive.Query();
+        queryTags.expand(expandTags);
+        var data = el.data("PBSLevel");
 
-                //TODO: use callbacks here so we can make sure the data is fully set before we bind.
+        // -- execute the query --
+        console.log("pbsid=" + SelectedPBS.Id);
+        data.expand(expandTags).getById(SelectedPBS.Id)
+            .then(function (data) {
+                    tagObjets = data.result.PBSItemTag; //JSON.parse(retrievedObject);
 
-                console.log("TRACE: [PBSItemDetail.show] Enter");
+                    //set icon for all objects -- probably be a better way to do this... 
+                    for (var i = 0; i < tagObjets.length; i++) {
+                        tagObjets[i].Icon_URL = "./Images/" + tagObjets[i].Classification + ".png";
+                    };
 
-                // -- build up the query for tags -- 
-                var expandTags = {
-                    "PBSItemTag": true
-                };
-                var queryTags = new Everlive.Query();
-                queryTags.expand(expandTags);
-                var data = el.data("PBSLevel");
-
-                // -- execute the query --
-                console.log("pbsid=" + SelectedPBS.Id);
-                data.expand(expandTags).getById(SelectedPBS.Id)
-                    .then(function (data) {
-                            tagObjets = data.result.PBSItemTag; //JSON.parse(retrievedObject);
-
-                            //set icon for all objects -- probably be a better way to do this... 
-                            for (var i = 0; i < tagObjets.length; i++) {
-                                tagObjets[i].Icon_URL = "./Images/" + tagObjets[i].Classification + ".png";
-                            };
-
-                            // create the item for binding purposes. 
-                            pbsItem =
-                                ({
-                                    name: SelectedPBS.Name,
-                                    type: SelectedPBS.Type,
-                                    Icon_URL: "./Images/" + SelectedPBS.Type + ".png",
-                                    pbsItemTags: tagObjets
-                                });
-
-                            //bind out to the pbsItemContent div
-                            kendo.bind($('#pbsItemContent'), pbsItem, kendo.mobile.ui);
-                        },
-                        function (error) {
-                            console.log(JSON.stringify(error));
+                    // create the item for binding purposes. 
+                    pbsItem =
+                        ({
+                            name: SelectedPBS.Name,
+                            type: SelectedPBS.Type,
+                            Icon_URL: "./Images/" + SelectedPBS.Type + ".png",
+                            pbsItemTags: tagObjets
                         });
 
-            },
-            hide: function () {},
-            openLink: function () {}
-        };
-    }());
+                    //bind out to the pbsItemContent div
+                    kendo.bind($('#pbsItemContent'), pbsItem, kendo.mobile.ui);
+                },
+                function (error) {
+                    console.log(JSON.stringify(error));
+                });
+
+    }
+    // (
+
+
+//     function () {
+//         var currentPBSItem;
+//         window.PBSItemDetail = {
+//             show: function () {
+
+//                 //TODO: use callbacks here so we can make sure the data is fully set before we bind.
+
+//                 console.log("TRACE: [PBSItemDetail.show] Enter");
+
+//                 // -- build up the query for tags -- 
+//                 var expandTags = {
+//                     "PBSItemTag": true
+//                 };
+//                 var queryTags = new Everlive.Query();
+//                 queryTags.expand(expandTags);
+//                 var data = el.data("PBSLevel");
+
+//                 // -- execute the query --
+//                 console.log("pbsid=" + SelectedPBS.Id);
+//                 data.expand(expandTags).getById(SelectedPBS.Id)
+//                     .then(function (data) {
+//                             tagObjets = data.result.PBSItemTag; //JSON.parse(retrievedObject);
+
+//                             //set icon for all objects -- probably be a better way to do this... 
+//                             for (var i = 0; i < tagObjets.length; i++) {
+//                                 tagObjets[i].Icon_URL = "./Images/" + tagObjets[i].Classification + ".png";
+//                             };
+
+//                             // create the item for binding purposes. 
+//                             pbsItem =
+//                                 ({
+//                                     name: SelectedPBS.Name,
+//                                     type: SelectedPBS.Type,
+//                                     Icon_URL: "./Images/" + SelectedPBS.Type + ".png",
+//                                     pbsItemTags: tagObjets
+//                                 });
+
+//                             //bind out to the pbsItemContent div
+//                             kendo.bind($('#pbsItemContent'), pbsItem, kendo.mobile.ui);
+//                         },
+//                         function (error) {
+//                             console.log(JSON.stringify(error));
+//                         });
+
+//             },
+//             hide: function () {},
+//             openLink: function () {}
+//         };
+//     }());
