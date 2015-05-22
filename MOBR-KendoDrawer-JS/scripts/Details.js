767feadaeeb25723451d0134b0ba9e55d882bd37
ahmiller42 <andrew.miller@intergraph.com>
2015-05-22 09:19:51 -0500

    Clean-up of details query related stuff. You can chain thens! YOU CAN CHAIN THENS!

--- MOBR-KendoDrawer-JS/scripts/Details.js
+++ MOBR-KendoDrawer-JS/scripts/Details.js
@@ -7,6 +7,8 @@ function setTaggedItemValues(callback) {
     queryDocuments.expand(expandDocuments);
     var documentData = el.data("TaggedItem");
     
+    docObjects = [];
+    localStorage.setItem('docObjects', JSON.stringify(docObjects));
     documentData.expand(expandDocuments).getById(SelectedObject.Id)
         .then(function (documentData) {
                 docObjects = documentData.result.TagDocument;
@@ -14,58 +16,70 @@ function setTaggedItemValues(callback) {
             },
             function (error) {
                 console.log(JSON.stringify(error));
-            });
-    
-    //
-    // -- build up the query for tags -- 
-    //
-    var expandTags = {
-        "TagToObservationReport": true
-    };
-    var queryTags = new Everlive.Query();
-    queryTags.expand(expandTags);
-    var data = el.data("TaggedItem");
+            })
+         .then(function () {
+             //
+             // -- build up the query for tags -- 
+             //
+             var expandTags = {
+                 "TagToObservationReport": true
+             };
+             var queryTags = new Everlive.Query();
+             queryTags.expand(expandTags);
+             var data = el.data("TaggedItem");
 
-    // -- execute the query --
-    data.expand(expandTags).getById(SelectedObject.Id)
-        .then(function (data) {
-            reportObjects = data.result.TagToObservationReport;
+             reportObjects = [];
+             localStorage.setItem('reportObjects', JSON.stringify(reportObjects));
+             // -- execute the query --
+             data.expand(expandTags).getById(SelectedObject.Id)
+                 .then(function (data) {
+                     reportObjects = data.result.TagToObservationReport;
 
-            //set icon for all objects -- probably be a better way to do this... 
-            for (var i = 0; i < reportObjects.length; i++) {
-                reportObjects[i].ClassType = "Observation Report";
-            };
+                     //set icon for all objects -- probably be a better way to do this... 
+                     for (var i = 0; i < reportObjects.length; i++) {
+                         reportObjects[i].ClassType = "Observation Report";
+                     };
 
-            // create the item for binding purposes. 
-            taggedItem =
-                ({
-                    name: SelectedObject.Name,
-                    header1: "Type",
-                    subItem1: SelectedObject.Classification,
-                    listHeader1: "Observation Reports",
-                    list1: reportObjects,
-                    documentListHeader: "Documents",
-                    documentList: JSON.parse(localStorage.getItem('docObjects')),
-                    launch_details: launch_details_function,
-                	file_doc: function (e) {
-                	    SelectedDoc = e.data;
+                     localStorage.setItem('reportObjects', JSON.stringify(reportObjects));
+                 })
+                .then(function () {
+                    // create the item for binding purposes. 
+                    taggedItem =
+                        ({
+                            name: SelectedObject.Name,
+                            header1: "Type",
+                            subItem1: SelectedObject.Classification,
+                            listHeader1: "Observation Reports",
+                            list1: JSON.parse(localStorage.getItem('reportObjects')),
+                            documentListHeader: "Documents",
+                            documentList: JSON.parse(localStorage.getItem('docObjects')),
+                            launch_details: launch_details_function,
+                            list2: [],
+                            list3: [],
+                            list4: [],
+                            list5: [],
+                            file_doc: function (e) {
+                                SelectedDoc = e.data;
 
-                	    var expandFiles = {
-                	        "DocumentFile": true
-                	    };
-                	    var queryFiles = new Everlive.Query();
-                	    queryFiles.expand(expandFiles);
-                	    var fileData = el.data("Documents");
-                	    fileData.expand(expandFiles).getById(SelectedDoc.Id)
-                            .then(function (fileData) {
-                                fileObjects = fileData.result.DocumentFile;
-                                window.open("https://bs3.cdn.telerik.com/v1/Hro5ZCMacvvdbWuA/" + fileObjects.Id, "_system");
-                            })
-                	}
+                                var expandFiles = {
+                                    "DocumentFile": true
+                                };
+                                var queryFiles = new Everlive.Query();
+                                queryFiles.expand(expandFiles);
+                                var fileData = el.data("Documents");
+                                fileData.expand(expandFiles).getById(SelectedDoc.Id)
+                                    .then(function (fileData) {
+                                        fileObjects = fileData.result.DocumentFile;
+                                        window.open("https://bs3.cdn.telerik.com/v1/Hro5ZCMacvvdbWuA/" + fileObjects.Id, "_system");
+                                    })
+                            }
+                        });
+
+                    callback(taggedItem);
                 });
 
-            callback(taggedItem);
-        })
+         });
+      
 }
 
 function bindToDetailsGrid(result) {
@@ -121,6 +135,8 @@ function setPBSItemValues(callback) {
     var queryChildren = new Everlive.Query();
     queryChildren.expand(expandChildren);
     var childData = el.data("PBSLevel");
+    childObjects = [];
+    localStorage.setItem('childObjects', JSON.stringify(childObjects));
     childData.expand(expandChildren).getById(SelectedObject.Id)
         .then(function (childData) {
             //alert(JSON.stringify(childData.result.Children));
@@ -133,48 +149,62 @@ function setPBSItemValues(callback) {
         },
         function (error) {
             console.log(JSON.stringify(error));
-        });
-
-
-    //
-    // -- build up the query for tags -- 
-    //
-    var expandTags = {
-        "PBSItemTag": true
-    };
-    var queryTags = new Everlive.Query();
-    queryTags.expand(expandTags);
-    var data = el.data("PBSLevel");
-
-    // -- execute the query --
-    console.log("pbsid=" + SelectedObject.Id);
-    data.expand(expandTags).getById(SelectedObject.Id)
-        .then(function (data) {
-            tagObjets = data.result.PBSItemTag; //JSON.parse(retrievedObject);
-
-            //set icon for all objects -- probably be a better way to do this... 
-            for (var i = 0; i < tagObjets.length; i++) {
-                tagObjets[i].ClassType = "Tag";
-                tagObjets[i].Icon_URL = "./Images/" + tagObjets[i].Classification + ".png";
+        })
+        .then(function () {
+            //
+            // -- build up the query for tags -- 
+            //
+            var expandTags = {
+                "PBSItemTag": true
             };
+            var queryTags = new Everlive.Query();
+            queryTags.expand(expandTags);
+            var data = el.data("PBSLevel");
 
-            // create the item for binding purposes. 
-            pbsItem =
-                ({
-                    name: SelectedObject.Name,
-                    header1: "Type",
-                    subItem1: SelectedObject.Type,
-                    Icon_URL: "./Images/" + SelectedObject.Type + ".png",
-                    listHeader1: "Tags",
-                    list1: tagObjets,
-                    listHeader2: "Children",
-                    list2: JSON.parse(localStorage.getItem('childObjects')),
-                    launch_details: launch_details_function
+            // -- execute the query --
+            console.log("pbsid=" + SelectedObject.Id);
+            tagObjects = [];
+            localStorage.setItem('tagObjects', JSON.stringify(tagObjects));
+            data.expand(expandTags).getById(SelectedObject.Id)
+                .then(function (data) {
+                    tagObjects = data.result.PBSItemTag; //JSON.parse(retrievedObject);
+
+                    //set icon for all objects -- probably be a better way to do this... 
+                    for (var i = 0; i < tagObjects.length; i++) {
+                        tagObjects[i].ClassType = "Tag";
+                        tagObjects[i].Icon_URL = "./Images/" + tagObjects[i].Classification + ".png";
+                    };
+
+                    localStorage.setItem('tagObjects', JSON.stringify(tagObjects));
+
+                },
+                function (error) {
+                    console.log(JSON.stringify(error));
+                })
+                .then(function () {
+
+                    // create the item for binding purposes. 
+                    pbsItem =
+                        ({
+                            name: SelectedObject.Name,
+                            header1: "Type",
+                            subItem1: SelectedObject.Type,
+                            Icon_URL: "./Images/" + SelectedObject.Type + ".png",
+                            listHeader1: "Tags",
+                            list1: JSON.parse(localStorage.getItem('tagObjects')),
+                            listHeader2: "Children",
+                            list2: JSON.parse(localStorage.getItem('childObjects')),
+                            launch_details: launch_details_function,
+                            list3: [],
+                            list4: [],
+                            list5: [],
+                            documentList: []
+                        });
+                    //console.log(pbsItem);
+
+
+
+                    callback(pbsItem);
                 });
-            //console.log(pbsItem);
-            callback(pbsItem);
-        },
-        function (error) {
-            console.log(JSON.stringify(error));
         });
 }
